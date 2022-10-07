de80d6c792b594c9fd9c9af616b4d0476ce97f8d
NicolasMoinet <7709392z@commun.ad.sncf.fr>
2022-10-07 14:29:48 +0200

    experimentation class css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+const App = () => {
+  const message: string = "Hello World";
+  const handleClick = (name: string) => alert(`Bonjour ${name} !`);
+  const handleClickSansPara = () => alert("WTF");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="color-pink">{message}</p>
+      <button onClick={() => handleClick("Nico")}>Test Nico</button>
+      {/* Tention, avec parametre on est obligé de rappeler la fonction avec ()=> voir ligne au dessus */}
+      <p>coucou</p>
+      {/* liaison de l'evenement click avc l appel de la fonction hanlCickSansParam sans parametre */}
+      <button onClick={handleClickSansPara}>Un autre boutton</button>
     </div>
   );
-}
+};
 
 export default App;
